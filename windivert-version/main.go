38c20ea2617cf160d2cbf9f2b1211387589f2da3
Rabbit Yilia <takatsuki@outlook.com>
2018-11-23 17:54:13 +0800

    Finish windivert version

--- windivert-version/main.go
+++ windivert-version/main.go
@@ -20,6 +20,8 @@ import (
 	"github.com/google/gopacket/layers"
 )
 
+var IPMap []net.IP
+
 func main() {
 	WindivertInit("./x86_64/WinDivert.dll")
 	Handle, err := WinDivertOpenGo("ipv6", 0, 0, 0)
@@ -27,6 +29,33 @@ func main() {
 		log.Fatal(err)
 	}
 
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, localnet192, err := net.ParseCIDR("192.168.0.0/16")
+	_, localnet10, err := net.ParseCIDR("10.0.0.0/8")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, address := range addrs {
+		thisaddr := address.(*net.IPNet)
+		if thisaddr.IP.IsLoopback() || !thisaddr.IP.IsGlobalUnicast() || thisaddr.IP.IsUnspecified() {
+			continue
+		}
+		if localnet10.Contains(thisaddr.IP) || localnet192.Contains(thisaddr.IP) {
+			continue
+		}
+		if strings.Contains(thisaddr.String(), ".") {
+			continue
+		}
+		IPMap = append(IPMap, thisaddr.IP)
+		log.Println("Listen on:", strings.Split(thisaddr.String(), "/")[0])
+	}
+	if len(IPMap) == 0 {
+		log.Fatal("No Address to listen")
+	}
 	go RXLoop(Handle)
 	TXLoop(Handle)
 	WinDivertCloseGo(Handle)
@@ -35,9 +64,13 @@ func main() {
 func TXLoop(Handle uintptr) {
 	buffer := gopacket.NewSerializeBuffer()
 	options := gopacket.SerializeOptions{}
-	SrcIP := "dddd:ace:cafe::5"
-	DstIP := "dddd:ace:cafe:1111:2222:3333:4444:5555"
+	input := GetInput("Dst IP")
+	DstIP := net.ParseIP(input)
+	if DstIP == nil {
+		log.Fatal("Invaild Addr")
+	}
 	for {
+		SrcIP := IPMap[RandInt(0, len(IPMap)-1)]
 		SrcPort := RandInt(1, 65535)
 		DstPort := RandInt(1, 65535)
 		err := buffer.Clear()
@@ -74,14 +107,14 @@ func TXLoop(Handle uintptr) {
 		UDPLayer := &layers.UDP{}
 		UDPLayer.SrcPort = layers.UDPPort(SrcPort)
 		UDPLayer.DstPort = layers.UDPPort(DstPort)
-		UDPLayer.Length = uint16(len(TXJson))
+		UDPLayer.Length = uint16(len(TXJson) + 8)
 
 		ipv6Layer := &layers.IPv6{}
-		ipv6Layer.SrcIP = net.ParseIP(SrcIP)
-		ipv6Layer.DstIP = net.ParseIP(DstIP)
+		ipv6Layer.SrcIP = SrcIP
+		ipv6Layer.DstIP = DstIP
 		ipv6Layer.Version = uint8(6)
 		ipv6Layer.HopLimit = uint8(64)
-		ipv6Layer.Length = uint16(UDPLayer.Length + 8)
+		ipv6Layer.Length = uint16(UDPLayer.Length)
 		ipv6Layer.NextHeader = layers.IPProtocolUDP
 
 		FakeHeader := makeUDPFakeHeader(SrcIP, DstIP, ipv6Layer.Length, SrcPort, DstPort, UDPLayer.Length)
@@ -93,8 +126,8 @@ func TXLoop(Handle uintptr) {
 
 		gopacket.SerializeLayers(buffer, options, ipv6Layer, UDPLayer)
 		TXPacket := append(buffer.Bytes(), TXJson...)
-		log.Println(hex.EncodeToString(TXPacket))
 		WinDivertHelperCalcChecksumsGo(Handle, TXPacket, TXAddr)
+
 		err = WinDivertSendGo(Handle, TXPacket, TXAddr)
 		if err != nil {
 			log.Fatal(err)
@@ -158,7 +191,7 @@ func ProcessRX(Handle uintptr, RXPacket []byte, RXAddr WinDivertAddress) {
 
 	if ThisRXPacket.ApplicationLayer() != nil {
 		RXdata := make(map[string]string)
-		err := json.Unmarshal(ThisRXPacket.ApplicationLayer().LayerContents(), &RXdata)
+		err := json.Unmarshal(ThisRXPacket.ApplicationLayer().Payload(), &RXdata)
 		if err == nil {
 			log.Println("From " + SrcIP.String() + "#" + SrcPort + " to " + DstIP.String() + "#" + DstPort + " :")
 			log.Println(RXdata)
@@ -176,13 +209,13 @@ func RandInt(min, max int) int {
 	return min + rand.Intn(max-min+1)
 }
 
-func makeUDPFakeHeader(SrcIPv6 string, DstIPv6 string, v6len uint16, SrcPort int, DstPort int, udplen uint16) string {
+func makeUDPFakeHeader(SrcIPv6 net.IP, DstIPv6 net.IP, v6len uint16, SrcPort int, DstPort int, udplen uint16) string {
 	UDPFakeHeader := ""
-	FakeUDPSrc, err := net.ParseIP(SrcIPv6).MarshalText()
+	FakeUDPSrc, err := SrcIPv6.MarshalText()
 	if err != nil {
 		log.Fatal(err)
 	}
-	FakeUDPDst, err := net.ParseIP(DstIPv6).MarshalText()
+	FakeUDPDst, err := DstIPv6.MarshalText()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -243,4 +276,22 @@ func checkSum(msg []byte) uint16 {
 	sum += (sum >> 16)
 	var ans = uint16(^sum)
 	return ans
+}
+
+func GetInput(tip string) string {
+	for {
+		log.Println("Please input " + tip + ":")
+		inputReader := bufio.NewReader(os.Stdin)
+		input, err := inputReader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.Trim(input, "\n")
+		input = strings.Trim(input, "\r")
+		if input == "" {
+			break
+		}
+		return input
+	}
+	return ""
 }
